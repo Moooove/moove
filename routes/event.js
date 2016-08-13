4252c05f17bea17261819477f145d9573b599f29
David Staehler <david.staehler@googlemail.com>
2016-08-13 19:46:47 +1200

    Updated event.js to save to database

--- routes/event.js
+++ routes/event.js
@@ -1,10 +1,25 @@
 var express = require('express');
+// models
+var Event = require('../models/event');
+
 // services
 var uuidService = require('../services/uuid');
 
 var router = express.Router();
-router.post('/event/new', function (req, res, next) {
-    console.log(req.body);
+router.post('/event/new', function (req, res) {
+    var title = req.body.title.trim();
+
+    var newEvent = new Event();
+    newEvent.title = title;
+
+    newEvent.save(function (err) {
+        if (err) {
+            console.log('save error', err);
+        } else {
+            console.log(req.body.title + " event created");
+        }
+    });
+
     var uuid = uuidService.newUuid();
     res.send({'uuid':uuid});
 });
