f0042d08c0f03a0e550d6106f4cae6542d81dc73
David Staehler <david.staehler@googlemail.com>
2016-08-13 15:58:55 +1200

    Connection to mongodb database

--- config.js
+++ config.js
@@ -1,4 +1,24 @@
-var config = new Object();
-Object.mongodb = 'http://summerOfTech:summeroftech2016@ds153845.mlab.com:53845/heroku_pp2t0tvn';
+var mongoose = require('mongoose');
 
-module.exports = config;
+/*
+ * Mongoose by default sets the auto_reconnect option to true.
+ * We recommend setting socket options at both the server and replica set level.
+ * We recommend a 30 second connection timeout because it allows for
+ * plenty of time in most operating environments.
+ */
+var options = { server: { socketOptions: { keepAlive: 300000, connectTimeoutMS: 30000 } },
+    replset: { socketOptions: { keepAlive: 300000, connectTimeoutMS : 30000 } } };
+
+var mongodbUri = 'http://summerOfTech:summeroftech2016@ds153845.mlab.com:53845/heroku_pp2t0tvn/links'
+;
+
+mongoose.connect(mongodbUri, options);
+var conn = mongoose.connection;
+
+conn.on('error', console.error.bind(console, 'connection error:'));
+
+conn.once('open', function() {
+    // Wait for the database connection to establish, then start the app.
+});
+
+ module.exports = config;
