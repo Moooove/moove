c4b02f006bb2040b03c2503d855d0025ba86f973
David Staehler <david.staehler@googlemail.com>
2016-08-14 13:20:31 +1200

    userlocation.js updated

--- models/user-location.js
+++ models/user-location.js
@@ -3,7 +3,7 @@ var mongoose = require('mongoose');
 var schema = mongoose.Schema({
     event_uuid:String,
     user_uuid: String,
-    timeout: {type:Date, expireAfterSeconds:200, default: Date.now()},
+    createdAt: {type:Date, expires:200, default: Date.now},
     lat: Number,
     long: Number
 });
