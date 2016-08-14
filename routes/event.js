09509946b603811591cb9bbd38914bcd6d36c2d2
Ian Weng <ian.weng@icloud.com>
2016-08-14 15:32:17 +1200

    fix bug for showing people in other event

--- routes/event.js
+++ routes/event.js
@@ -48,8 +48,9 @@ router.get('/event/:uuid',function (req,res) {
             console.log(err)
             return console.error(err);
         } else {
+            console.log(event)
             var user_uuid = uuidService.newUuid;
-            res.render('event',{user_uuid:user_uuid, event_uuid:event.event_uuid});
+            res.render('event',{user_uuid:user_uuid, event_uuid:event[0].uuid});
         }
     });
 });
